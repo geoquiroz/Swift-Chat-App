067bb27ca0ea46baca48dd5b02a9d9c6c5cc0ee2
geovanny <gquiroz@hireology.com>
2021-11-11 10:23:51 -0500

    Initial Commit

---
+++ swift-chat-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-chat-app
+//
+//  Created by Geovanny Quiroz on 11/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift-chat-app/swift_chat_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_chat_appApp.swift
+//  swift-chat-app
+//
+//  Created by Geovanny Quiroz on 11/11/21.
+//
+
+import SwiftUI
+
+@main
+struct swift_chat_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
